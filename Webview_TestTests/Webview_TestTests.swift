2595dabfad6de45fd3e35e2246ba046065812181
Andreas Enz <andi_enz@msn.com>
2016-10-26 15:09:29 +0200

    first commit

---
+++ Webview_TestTests/Webview_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  Webview_TestTests.swift
+//  Webview_TestTests
+//
+//  Created by Enz  Andreas on 26.10.16.
+//  Copyright © 2016 COSS. All rights reserved.
+//
+
+import XCTest
+@testable import Webview_Test
+
+class Webview_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
